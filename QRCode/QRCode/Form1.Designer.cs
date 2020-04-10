c662a3f423ab42613cc5bea6f346c624514a437d
Bruno Henrique <bruno_hcs2009@hotmail.com>
2020-04-10 12:32:41 -0300

    Commit full

---
+++ QRCode/QRCode/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace QRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Gerar = new System.Windows.Forms.Button();
+            this.txt_Texto = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Gerar
+            // 
+            this.btn_Gerar.Location = new System.Drawing.Point(188, 244);
+            this.btn_Gerar.Name = "btn_Gerar";
+            this.btn_Gerar.Size = new System.Drawing.Size(99, 23);
+            this.btn_Gerar.TabIndex = 0;
+            this.btn_Gerar.Text = "Gerar QRCode";
+            this.btn_Gerar.UseVisualStyleBackColor = true;
+            this.btn_Gerar.Click += new System.EventHandler(this.btn_Gerar_Click);
+            // 
+            // txt_Texto
+            // 
+            this.txt_Texto.Location = new System.Drawing.Point(82, 244);
+            this.txt_Texto.Name = "txt_Texto";
+            this.txt_Texto.Size = new System.Drawing.Size(100, 20);
+            this.txt_Texto.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite algo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Seu QRCode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 289);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_Texto);
+            this.Controls.Add(this.btn_Gerar);
+            this.Name = "Form1";
+            this.Text = "QRCode";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Gerar;
+        private System.Windows.Forms.TextBox txt_Texto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QRCode/QRCode/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Gerar_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator QR = new QRCoder.QRCodeGenerator();
+            var texto = QR.CreateQrCode(txt_Texto.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(texto);
+            pictureBox1.Image = code.GetGraphic(50);
+            //txt_Texto.Text = null;
+        }
+    }
+}
